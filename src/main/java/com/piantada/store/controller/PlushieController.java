06ae95f707d3ce37c2c437b6fb55c69dc56558c2
vieradaniel <dariennx@hotmail.com>
2021-03-20 09:21:40 -0300

    CRUD edit and delete added

--- src/main/java/com/piantada/store/controller/PlushieController.java
+++ src/main/java/com/piantada/store/controller/PlushieController.java
@@ -1,16 +1,19 @@
 package com.piantada.store.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.piantada.store.model.Plushie;
 import com.piantada.store.repo.PlushieRepo;
@@ -34,6 +37,15 @@ public class PlushieController {
 		return "index";
 	}
 	
+	@RequestMapping("/catalogo")
+	public String listView(Model model) {
+		
+		List<Plushie> plushieList = service.listAll();
+		model.addAttribute("plushies",plushieList);
+		
+		return "listView";
+	}
+	
 	@RequestMapping("/new")
 	public String showNewPlushieForm(Model model) {
 		
@@ -51,6 +63,20 @@ public class PlushieController {
 	}
 	
 	
+	@RequestMapping("/edit/{id}")
+	public ModelAndView showEditPlushieForm(@PathVariable(name= "id") Long id) {
+		
+		ModelAndView mav = new ModelAndView("edit_plushie");
+		Optional<Plushie> plushie = service.get(id);
+		mav.addObject("plushie", plushie);
+		
+		return mav;
+	}
 	
+	@RequestMapping("/delete/{id}")
+	public String deletePlushie(@PathVariable (name="id") Long id){
+		service.delete(id);
+		return "redirect:/";
+	}
 	
 }
